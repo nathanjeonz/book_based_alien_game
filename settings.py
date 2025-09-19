c89f71f5dcb0eb62a977f06998409c93c4fa3f6c
nathanjeonz <jeonz0102_gmail.com>
2025-09-19 19:43:04 +0700

    problem bullets are not visible

--- settings.py
+++ settings.py
@@ -5,11 +5,11 @@ class Settings():
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 255, 255)
+        self.bg_color = (255, 255, 255)
 
         self.ship_speed_factor = 1
 
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (50,50,50)
+        self.bullet_color = (0,0,0)
